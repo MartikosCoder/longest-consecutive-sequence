3b2fdc66290b14831ee6690b96567e60d37965ba
Martin <martbelmoaw@gmail.com>
2018-09-12 17:36:01 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    if(array.length === 0){
+        return 0;
+    } else {
+        array.sort((left, right) => left - right);
+
+        let longest_streak = 1,
+            current_streak = 1;
+
+        for(let i = 1; i < array.length; i++){
+            if(array[i] !== array[i - 1]){
+                if(array[i] === array[i - 1] + 1){
+                    current_streak++;
+                } else {
+                    longest_streak = Math.max(longest_streak, current_streak);
+                    current_streak = 1;
+                }
+            }
+        }
+
+        return Math.max(longest_streak, current_streak);
+    }
 }
