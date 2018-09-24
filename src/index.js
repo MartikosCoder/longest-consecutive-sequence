704da2f077361a09ad52b528cc4d878deb76f818
Martin <martbelmoaw@gmail.com>
2018-09-24 20:56:08 +0300

    refactored

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ module.exports = function longestConsecutiveLength(array) {
     if(array.length === 0){
         return 0;
     } else {
-        array.sort((left, right) => left - right);
+        const work_arr = [...array];
+        work_arr.sort((left, right) => left - right);
 
         let longest_streak = 1,
             current_streak = 1;
 
-        for(let i = 1; i < array.length; i++){
-            if(array[i] !== array[i - 1]){
-                if(array[i] === array[i - 1] + 1){
+        for(let i = 1; i < work_arr.length; i++){
+            if(work_arr[i] !== work_arr[i - 1]){
+                if(work_arr[i] === work_arr[i - 1] + 1){
                     current_streak++;
                 } else {
                     longest_streak = Math.max(longest_streak, current_streak);
